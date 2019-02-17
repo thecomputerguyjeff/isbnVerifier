d016e944ea4d1fa8fabe686bce0588f701ff62b0
Jeff Mayer <TheComputerGuy.Jeff@gmail.com>
2019-02-17 11:28:44 -0600

    first commit

---
+++ src/main/java/IsbnVerifier.java
@@ -0,0 +1,10 @@
+class IsbnVerifier {
+
+    boolean isValid(String stringToVerify) {
+
+
+
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
