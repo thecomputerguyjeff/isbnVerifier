3b51b131c96dbf634c4f63d1e6d57088386d814a
Malka Bachrach <malka.bachrach@ti.htc.edu>
2019-02-23 20:02:18 -0600

    first commit

--- src/main/java/IsbnVerifier.java
+++ src/main/java/IsbnVerifier.java
@@ -1,10 +1,28 @@
-class IsbnVerifier {
+
+class IsbnVerifier extends IsbnVarifierMethods{
+    private IsbnVarifierMethods Varifier=new IsbnVarifierMethods();
 
     boolean isValid(String stringToVerify) {
+        Integer number=null;
+        Integer isbn=0;
+        Integer y=0;
+        String character;
+        for(int i=0; i<stringToVerify.length();i++) {
+            character=stringToVerify.substring(i,i+1);
+            if(!(character.equals("-"))) {
+                number = Varifier.parseIt(character, i, stringToVerify);
 
-
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+                if (number == null) {
+                    return false;
+                }
+                isbn = isbn + (number * (10 - y));
+                y++;
+            }
+        }
+        if(y!=10){
+            return false;
+        }
+        return Varifier.testModEleven(isbn);
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
-
 }
